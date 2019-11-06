0d07766a41db847a11e0e8f70a4536569eaab27e
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2019-11-06 19:53:03 +0200

    Break code style rules

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,7 +4,9 @@ import ohtu.ohtuvarasto.Varasto;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    
+    {
 
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
